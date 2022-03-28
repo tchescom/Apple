5b89aaa3852e298ba4683e0b3c7f4e5c7d6a74ec
tchescoM <tchesco@live.nl>
2022-03-27 22:01:29 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const menu_ = document.querySelector('.menu');
+const search_ = document.querySelectorAll('.fa-search');
+const header_ = document.querySelector('header');
+function search_click(){
+    header_.style.width = '8px';
+}
